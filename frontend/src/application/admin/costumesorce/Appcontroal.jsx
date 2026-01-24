361f794f24d3c3df10661b9463e7da0f12a35acd
divakar900164 <divakarbug01@gmail.com>
2026-01-25 03:39:27 +0530

    product

--- frontend/src/application/admin/costumesorce/Appcontroal.jsx
+++ frontend/src/application/admin/costumesorce/Appcontroal.jsx
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:5500';
-// export const API_URL = 'https://user-database-9uyd.onrender.com';
+// export const API_URL = 'http://localhost:5500';
+export const API_URL = 'https://user-database-9uyd.onrender.com';
